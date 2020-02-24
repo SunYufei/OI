0d901d7c09b3a5937d4f056dc6c0df70d9cd7435
SunYufei <sunyufei963@gmail.com>
2020-02-24 20:23:52 +0800

    update: 2020/2/24

---
+++ CCF/CSP/2013/J121.java
@@ -0,0 +1,5 @@
+class J13121 {
+    public static void main(String[] args) {
+
+    }
+}
--- LeetCode/editor/cn/[125]验证回文串.cpp
+++ LeetCode/editor/cn/[125]验证回文串.cpp
@@ -1,13 +1,24 @@
-/*
- * @lc app=leetcode.cn id=125 lang=cpp
- *
- * [125] 验证回文串
- */
-#include <bits/stdc++.h>
-using namespace std;
+//给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。 
+//
+// 说明：本题中，我们将空字符串定义为有效的回文串。 
+//
+// 示例 1: 
+//
+// 输入: "A man, a plan, a canal: Panama"
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 输入: "race a car"
+//输出: false
+// 
+// Related Topics 双指针 字符串
 
+
+//leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
-  public:
+public:
     bool isPalindrome(string s) {
         size_t len = s.length();
         if (len == 0 || len == 1)
@@ -35,3 +46,4 @@ class Solution {
         return true;
     }
 };
+//leetcode submit region end(Prohibit modification and deletion)
