ee544292d5b90da479f7d85ceffd48a4ca6735ca
SunYufei <sunyufei963@gmail.com>
2020-03-13 11:38:19 +0800

    Update: 2020/03/13

--- LeetCode/Java/242-hash-table-有效的字母异位词.java
+++ LeetCode/Java/242-hash-table-有效的字母异位词.java
@@ -7,8 +7,17 @@
 // @lc code=start
 class Solution {
     public boolean isAnagram(String s, String t) {
-
+        if (s.length() != t.length())
+            return false;
+        int[] table = new int[256];
+        for (char i : s.toCharArray())
+            table[i]++;
+        for (char i : t.toCharArray()) {
+            table[i]--;
+            if (table[i] < 0)
+                return false;
+        }
+        return true;
     }
 }
 // @lc code=end
-
