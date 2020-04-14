3ab077b4b2027d2d40a6c81966404c05ceb49d0c
SunYufei <sunyufei963@gmail.com>
2020-04-14 20:31:58 +0800

    Update: 2020/04/14

--- CodingInterviews/NowCoder/Find.cpp
+++ CodingInterviews/NowCoder/Find.cpp
@@ -3,10 +3,9 @@
 class Solution {
   public:
     bool Find(int target, vector<vector<int>> array) {
-        int m = array.size();
-        if (m == 0)
+        if (array.size() == 0)
             return false;
-        int n = array[0].size();
+        int m = array.size(), n = array[0].size();
         int i = 0, j = n - 1;
         while (i < m && j >= 0) {
             if (array[i][j] == target)
