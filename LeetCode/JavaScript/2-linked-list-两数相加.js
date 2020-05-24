a60b05de3f452306722a7b454b5fe33395adcf25
SunYufei <sunyufei963@gmail.com>
2020-05-24 20:14:50 +0800

    Update: 2020/05/24

--- LeetCode/JavaScript/2-linked-list-两数相加.js
+++ LeetCode/JavaScript/2-linked-list-两数相加.js
@@ -19,12 +19,12 @@ import { ListNode } from './main.js';
  */
 var addTwoNumbers = function (l1, l2) {
     let res = new ListNode(null);
-    let t = res, carry = 0, sum = 0;
+    let t = res, carry = 0;
     while (l1 || l2) {
-        sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
-        t.next = new ListNode(sum % 10);
+        carry = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+        t.next = new ListNode(carry % 10);
         t = t.next;
-        carry = sum >= 10 ? 1 : 0;
+        carry = carry >= 10 ? 1 : 0;
         l1 && (l1 = l1.next);
         l2 && (l2 = l2.next);
     }
